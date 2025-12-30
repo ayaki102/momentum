6106eb0bcf0fc1c8448c406cf3210bddb1dc7995
ayaki102 <minecraftowykoks420@gmail.com>
2025-12-30 07:23:41 +0100

    this is just todo app, basic functionality done with CRUD

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+from os import stat
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -52,7 +53,10 @@ async def get_tasks(request: Request):
 
 @app.get("/task/{id}", response_class=HTMLResponse)
 async def get_task(request: Request, id: int):
-    task = tasks[id - 1]
+    task = next((t for t in tasks if t[ID] == id), None)
+
+    if not task:
+        return HTMLResponse("task not found", status_code=404)
     return templates.TemplateResponse(
         request=request, name="task.html", context={"task": task}
     )
@@ -60,12 +64,40 @@ async def get_task(request: Request, id: int):
 
 @app.post("/task/status_update/{id}")
 async def complete_task(id: int, request: Request):
-    if id < 1 or id > len(tasks):
-        return HTMLResponse(content="Task not found", status_code=404)
-    if tasks[id - 1][COMPLETED]:
-        tasks[id - 1][COMPLETED] = False
-    else:
-        tasks[id - 1][COMPLETED] = True
+    task = next((t for t in tasks if t[ID] == id), None)
+
+    if not task:
+        return HTMLResponse("Task not found", status_code=404)
+
+    task[COMPLETED] = not task[COMPLETED]
+
+    next_url = request.query_params.get("next", "/task")
+    return RedirectResponse(next_url, status_code=303)
+
+
+@app.post("/task/create")
+async def create_task(
+    name: str = Form(...),
+    date: str = Form(...),
+    time: str = Form(...),
+):
+    new_task = {
+        NAME: name,
+        DATE: f"{date} {time}",
+        COMPLETED: False,
+        ID: len(tasks) + 1,
+    }
+
+    tasks.append(new_task)
+    return RedirectResponse(url="/task", status_code=303)
+
+
+@app.post("/task/delete/{id}")
+async def delete_task(id: int, request: Request):
+    for i, task in enumerate(tasks):
+        if task[ID] == id:
+            tasks.pop(i)
+            break
 
     next_url = request.query_params.get("next", "/task")
     return RedirectResponse(url=next_url, status_code=303)
